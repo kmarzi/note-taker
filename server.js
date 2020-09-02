730d315f7a3dc87e8531ebb483236af0e720d631
Kmarzi <k.l.marziali@gmail.com>
2020-09-02 19:49:59 -0400

    edited PORT with process

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const uniqID =require("uniqid");
 const fs = require("fs");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true }));
